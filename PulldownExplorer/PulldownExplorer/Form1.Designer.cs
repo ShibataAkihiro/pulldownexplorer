aa13c6316c8136e45564999fc68bc83d6ef4fbb9
Akihiro Shibata <akihiroshbt2@gmail.com>
2015-08-05 15:07:27 +0900

    レイアウト検討

---
+++ PulldownExplorer/PulldownExplorer/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace PulldownExplorer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.編集ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.カットToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.カットToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ペーストToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.folderListTreeView = new System.Windows.Forms.TreeView();
+            this.fileBrowserListView = new System.Windows.Forms.ListView();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 251);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(292, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.編集ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 編集ToolStripMenuItem
+            // 
+            this.編集ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.カットToolStripMenuItem,
+            this.カットToolStripMenuItem1,
+            this.ペーストToolStripMenuItem});
+            this.編集ToolStripMenuItem.Name = "編集ToolStripMenuItem";
+            this.編集ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.編集ToolStripMenuItem.Text = "編集";
+            // 
+            // カットToolStripMenuItem
+            // 
+            this.カットToolStripMenuItem.Name = "カットToolStripMenuItem";
+            this.カットToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.カットToolStripMenuItem.Text = "コピー";
+            // 
+            // カットToolStripMenuItem1
+            // 
+            this.カットToolStripMenuItem1.Name = "カットToolStripMenuItem1";
+            this.カットToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.カットToolStripMenuItem1.Text = "カット";
+            // 
+            // ペーストToolStripMenuItem
+            // 
+            this.ペーストToolStripMenuItem.Name = "ペーストToolStripMenuItem";
+            this.ペーストToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ペーストToolStripMenuItem.Text = "ペースト";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.folderListTreeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fileBrowserListView);
+            this.splitContainer1.Size = new System.Drawing.Size(292, 227);
+            this.splitContainer1.SplitterDistance = 97;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // folderListTreeView
+            // 
+            this.folderListTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.folderListTreeView.Location = new System.Drawing.Point(0, 0);
+            this.folderListTreeView.Name = "folderListTreeView";
+            this.folderListTreeView.Size = new System.Drawing.Size(97, 227);
+            this.folderListTreeView.TabIndex = 0;
+            // 
+            // fileBrowserListView
+            // 
+            this.fileBrowserListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileBrowserListView.Location = new System.Drawing.Point(0, 0);
+            this.fileBrowserListView.Name = "fileBrowserListView";
+            this.fileBrowserListView.Size = new System.Drawing.Size(191, 227);
+            this.fileBrowserListView.TabIndex = 0;
+            this.fileBrowserListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(114, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(114, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Explorer";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 編集ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem カットToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem カットToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ペーストToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView folderListTreeView;
+        private System.Windows.Forms.ListView fileBrowserListView;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ PulldownExplorer/PulldownExplorer/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PulldownExplorer
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            var rootItem = new ANode();
+            rootItem.Text = "asdf";
+            folderListTreeView.Nodes.Add(rootItem);
+        }
+
+    }
+
+    public class ANode : TreeNode
+    {
+        public ANode() { }
+    }
+    public class FileSystemNode : TreeNode
+    {
+        public System.IO.DirectoryInfo Directory { get; }
+        public bool Exist => this.Directory.Exists;
+        public new List<FileSystemNode> Nodes = new List<FileSystemNode>();
+        public FileSystemNode(string path)
+        {
+            Directory = new System.IO.DirectoryInfo(path);
+            Text = Directory.Name;
+            foreach (var d in Directory.GetDirectories())
+            {
+                Nodes.Add(new FileSystemNode(d.FullName));
+            }
+        }
+
+    }
+}
